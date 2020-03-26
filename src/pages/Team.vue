804ca8002137be8a0ddd48d6221bd52fa469c592
Deta Utama <deta@oddbit.id>
2020-03-26 11:37:53 +0800

    Adding image link to asset

--- src/pages/Team.vue
+++ src/pages/Team.vue
@@ -1,16 +1,12 @@
 <template>
   <Layout>
-    <div
-      class="container mx-auto px-6 lg:px-8 flex flex-col items-center my-16"
-    >
+    <div class="container mx-auto px-6 lg:px-8 flex flex-col items-center my-16">
       <div class="my-6 flex flex-col items-center">
-        <h1 class="uppercase text-4xl font-bold tracking-wider text-gray-800">
-          Our team
-        </h1>
+        <h1 class="uppercase text-4xl font-bold tracking-wider text-gray-800">Our team</h1>
         <span class="border-b-2 border-primary w-10 my-2"></span>
-        <p class="text-gray-800 text-lg font-light text-center">
-          Our team based in Bali with remote team in Jakarta
-        </p>
+        <p
+          class="text-gray-800 text-lg font-light text-center"
+        >Our team based in Bali with remote team in Jakarta</p>
       </div>
       <div class="flex flex-wrap -mx-8 mt-10">
         <div
@@ -31,9 +27,7 @@
           <p class="text-gray-700 font-light">{{ person.description }}</p>
         </div>
         <!-- Next team -->
-        <div
-          class="flex flex-col w-full md:w-1/3 lg:w-1/4 items-center p-8 lg:py-12 text-center"
-        >
+        <div class="flex flex-col w-full md:w-1/3 lg:w-1/4 items-center p-8 lg:py-12 text-center">
           <g-image
             src="~/assets/images/no-avatar.jpg"
             alt="Oddbit next team"
@@ -42,13 +36,9 @@
             class="w-32 h-32 rounded-full border-4 border-white mb-1"
           />
           <h4 class="font-bold capitalize">Are you the next?</h4>
-          <g-link to="/career" class="font-light text-secondary"
-            >Yes, I am</g-link
-          >
+          <g-link to="/career" class="font-light text-secondary">Yes, I am</g-link>
           <span class="border-b border-gray-500 w-10 my-2"></span>
-          <p class="text-gray-700 font-light">
-            Be a part of our team and grow together.
-          </p>
+          <p class="text-gray-700 font-light">Be a part of our team and grow together.</p>
         </div>
       </div>
     </div>
@@ -57,6 +47,7 @@
 
 <script>
 import noAvatar from "~/assets/images/no-avatar.jpg";
+import Deta from "~/assets/images/teams/deta.jpg";
 
 export default {
   metaInfo: {
@@ -117,9 +108,8 @@ export default {
         {
           name: "Deta Utama",
           position: "Web Developer",
-          description:
-            "There is Life After Failure",
-          picture: noAvatar
+          description: "There is Life After Failure",
+          picture: Deta
         },
         {
           name: "Wida",
