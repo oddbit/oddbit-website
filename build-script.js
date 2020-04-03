edbf4593ad7b744420dddded9eb43abf1e362732
Ananda Widiprabawa <ananda.widiprabawa@gmail.com>
2020-04-03 21:24:29 +0800

    build(): make copy file synchronously

--- build-script.js
+++ build-script.js
@@ -6,15 +6,11 @@ const path = require('path')
  * `production` or `development`.
  */
 const APP_ENV = process.env.APP_ENV;
-console.log('APP_ENV', APP_ENV);
 
 // Handle copying robots.txt file
 const assetsDir = path.join(__dirname, 'src', 'assets');
 const robotsFileName = APP_ENV === 'production' ? 'robots.txt' : 'robots.dev.txt';
 const robotsSource = path.join(assetsDir, robotsFileName);
 const robotsDest = path.join(__dirname, 'static', 'robots.txt')
-console.log('Copying robots file')
-console.log('Assets directory', assetsDir);
-console.log('Robots source', robotsSource);
-console.log('Robots destination', robotsDest);
-fs.copyFile(robotsSource, robotsDest, () => {});
+fs.copyFileSync(robotsSource, robotsDest);
+console.log('robots.txt copied');
