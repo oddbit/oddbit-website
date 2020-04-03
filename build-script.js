4dd39e46df49cac575ed00fafc4778987585276b
Ananda Widiprabawa <ananda.widiprabawa@gmail.com>
2020-04-03 20:01:27 +0800

    build(): make script for build process
    use conditional file (robots.txt) between staging and production

---
+++ build-script.js
@@ -0,0 +1,15 @@
+const fs = require('fs')
+const path = require('path')
+
+/**
+ * App environment.
+ * `production` or `development`.
+ */
+const APP_ENV = process.env.APP_ENV;
+
+// Handle copying robots.txt file
+const assetsDir = path.join(__dirname, 'src', 'assets');
+const robotsFileName = APP_ENV === 'production' ? 'robots.txt' : 'robots.dev.txt';
+const robotsSource = path.join(assetsDir, robotsFileName);
+const robotsDest = path.join(__dirname, 'static', 'robots.txt')
+fs.copyFile(robotsSource, robotsDest, () => {});
